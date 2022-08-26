8466df47bd4bd824d19cc22cdc8722cdfef5f313
ngocthieuff <ptnngoc22@gmail.com>
2022-08-26 16:05:42 +0700

    show func and public variables after deploying

---
+++ Lesson 2: Welcome to Remix! Simple Storage/SimpleStorage.sol
@@ -0,0 +1,24 @@
+// But, maybe we're okay if we use a more new version of solidity than 0.8.7
+// to tell our code that we're okay with a more new version, we can put a little caret here. 
+// Hey, any version of 0.8.7 and above is okay for this contract.  
+// Use pragma solidity ^0.8.7;
+
+// Just 0.8.7, use pragma solidity 0.8.7;
+
+// Or with range, pragma solidity >=0.8.7 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+// To start defining our contract, we're gonna go ahead and write the word contract
+// This tell solidity that the next pieces of code is going to be a contract.
+contract SimpleStorage {
+    // basic types: boolean, uint, int, address, bytes
+    // uint256 favoriteNumber;
+
+    // public keyword made it visible after deploying
+    uint256 public favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+}
