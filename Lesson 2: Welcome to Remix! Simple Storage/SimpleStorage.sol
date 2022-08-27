e23f855d51331c41cdda62704ca1f91010527989
ngocthieuff <ptnngoc22@gmail.com>
2022-08-27 10:15:51 +0700

    add retrieve func

--- Lesson 2: Welcome to Remix! Simple Storage/SimpleStorage.sol
+++ Lesson 2: Welcome to Remix! Simple Storage/SimpleStorage.sol
@@ -9,17 +9,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-// To start defining our cont// But, maybe we're okay if we use a more new version of solidity than 0.8.7
-// to tell our code that we're okay with a more new version, we can put a little caret here. 
-// Hey, any version of 0.8.7 and above is okay for this contract.  
-// Use pragma solidity ^0.8.7;
-
-// Just 0.8.7, use pragma solidity 0.8.7;
-
-// Or with range, pragma solidity >=0.8.7 <0.9.0;
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.8;
-
 // To start defining our contract, we're gonna go ahead and write the word contract
 // This tell solidity that the next pieces of code is going to be a contract.
 contract SimpleStorage {
@@ -39,17 +28,5 @@ contract SimpleStorage {
 
     function retrieve() public view returns(uint256){
         return favoriteNumber;
-    }
-}ract, we're gonna go ahead and write the word contract
-// This tell solidity that the next pieces of code is going to be a contract.
-contract SimpleStorage {
-    // basic types: boolean, uint, int, address, bytes
-    // uint256 favoriteNumber;
-
-    // public keyword made it visible after deploying
-    uint256 public favoriteNumber;
-
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
     }
 }
